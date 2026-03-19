0eb9662de29e6b90370306e18136a9d20c70bb9c
Ayush <ayushjadaun6@gmail.com>
2026-03-19 21:22:45 +0530

    feat: verifier GitHub avatar as badge icon with profile links

--- docs/.vitepress/theme/components/VerificationBadge.vue
+++ docs/.vitepress/theme/components/VerificationBadge.vue
@@ -25,16 +25,17 @@ const verifierLinkedin = computed(() => frontmatter.value.verifierLinkedin || ''
       </svg>
 
       <template v-if="isVerified">
-        <span class="badge-text">Verified by <strong>{{ verifiedBy }}</strong></span>
-        <span v-if="verifiedDate" class="badge-date">· {{ verifiedDate }}</span>
-        <span class="badge-links">
-          <a v-if="verifierGithub" :href="`https://github.com/${verifierGithub}`" target="_blank" title="GitHub" class="profile-link">
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z"/></svg>
-          </a>
-          <a v-if="verifierLinkedin" :href="`https://linkedin.com/in/${verifierLinkedin}`" target="_blank" title="LinkedIn" class="profile-link">
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
-          </a>
+        <a v-if="verifierGithub" :href="`https://github.com/${verifierGithub}`" target="_blank" class="verifier-avatar" :title="verifiedBy">
+          <img :src="`https://github.com/${verifierGithub}.png?size=40`" :alt="verifiedBy" width="20" height="20" />
+        </a>
+        <span class="badge-text">Verified by
+          <a v-if="verifierGithub" :href="`https://github.com/${verifierGithub}`" target="_blank" class="verifier-name">{{ verifiedBy }}</a>
+          <strong v-else>{{ verifiedBy }}</strong>
         </span>
+        <span v-if="verifiedDate" class="badge-date">· {{ verifiedDate }}</span>
+        <a v-if="verifierLinkedin" :href="`https://linkedin.com/in/${verifierLinkedin}`" target="_blank" title="LinkedIn" class="profile-link">
+          <svg width="13" height="13" viewBox="0 0 24 24" fill="currentColor"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
+        </a>
       </template>
 
       <template v-else>
@@ -76,14 +77,31 @@ const verifierLinkedin = computed(() => frontmatter.value.verifierLinkedin || ''
   flex-wrap: wrap;
 }
 
-.badge-date {
-  opacity: 0.7;
+.verifier-avatar {
+  display: inline-flex;
+  border-radius: 50%;
+  overflow: hidden;
+  border: 1.5px solid #22c55e;
+  flex-shrink: 0;
 }
 
-.badge-links {
-  display: flex;
-  gap: 6px;
-  margin-left: 4px;
+.verifier-avatar img {
+  display: block;
+  border-radius: 50%;
+}
+
+.verifier-name {
+  color: #22c55e;
+  font-weight: 600;
+  text-decoration: none;
+}
+
+.verifier-name:hover {
+  text-decoration: underline;
+}
+
+.badge-date {
+  opacity: 0.7;
 }
 
 .profile-link {
@@ -92,6 +110,7 @@ const verifierLinkedin = computed(() => frontmatter.value.verifierLinkedin || ''
   color: var(--vp-c-text-2);
   opacity: 0.6;
   transition: opacity 0.15s;
+  margin-left: 2px;
 }
 
 .profile-link:hover {
